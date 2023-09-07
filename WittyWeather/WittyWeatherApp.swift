fbb32fb2ba12a9a16ff7ddeea6d15f0ca8d79804
Felipe Lara <felipe.lara@farfetch.com>
2023-09-07 18:09:13 +0100

    Initial Commit

---
+++ WittyWeather/WittyWeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  WittyWeatherApp.swift
+//  WittyWeather
+//
+//  Created by Felipe Lara on 07/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WittyWeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
